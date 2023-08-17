8b05efee51011ee3942aeb2dba84eff1d981c098
devSveaVaccin <132374394+devSveaVaccin@users.noreply.github.com>
2023-08-17 13:47:08 +0200

    Update estimat_pris.js
    denna kod ska visa "TEST OK" så fort man klickar på "Se vacciner och pris" knappen.

--- estimat_pris.js
+++ estimat_pris.js
@@ -1,23 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('[contact-form-7 id="20930" title="Priskollen"]');
-    const outputElement = document.createElement('div');
-    form.appendChild(outputElement);
+  const form = document.querySelector('[contact-form-7 id="20930" title="Priskollen"]');
+  const outputElement = document.querySelector('#estimat_output');
 
-    const submitButton = form.querySelector('[type="submit"]');
-    submitButton.addEventListener('click', function (event) {
-        event.preventDefault();
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-        const country = form.querySelector('select[name="country"]').value;
-        const duration = form.querySelector('input[name="duration"]:checked').value;
-        const stayType = form.querySelector('input[name="stay_type"]:checked').value;
-
-        const estimat = räknaUtEstimat(country, duration, stayType);
-        outputElement.innerHTML = estimat;
-    });
+    outputElement.innerHTML = "TEST OK"; // Visa "TEST OK" när knappen klickas på.
+  });
 });
-
-function räknaUtEstimat(country, duration, stayType) {
-    // Här kan du implementera din logik för att räkna ut estimatet baserat på användarens val
-    // Returnera resultatet som en sträng
-    return "Estimerat pris och vaccininformation kommer här.";
-}
