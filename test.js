b872776b8891c7d4c8fbc74a9cb37a62529f7eaf
devSveaVaccin <132374394+devSveaVaccin@users.noreply.github.com>
2023-08-17 12:50:27 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('[title="Priskollen"]');
+    const outputElement = document.createElement('div');
+    form.appendChild(outputElement);
+
+    const submitButton = form.querySelector('[type="submit"]');
+    submitButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const country = form.querySelector('select[name="country"]').value;
+        const duration = form.querySelector('input[name="duration"]:checked').value;
+        const stayType = form.querySelector('input[name="stay_type"]:checked').value;
+
+        const estimat = räknaUtEstimat(country, duration, stayType);
+        outputElement.innerHTML = estimat;
+    });
+});
+
+function räknaUtEstimat(country, duration, stayType) {
+    // Här kan du implementera din logik för att räkna ut estimatet baserat på användarens val
+    // Returnera resultatet som en sträng
+    return "Estimerat pris och vaccininformation kommer här.";
+}
